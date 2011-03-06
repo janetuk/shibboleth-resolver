22065b0b5976a56b3be02ac9bc8c14b53a6f4088
scantor <scantor@3ebe4818-3638-0410-822d-ece5924dabe2>
2011-03-06 20:30:22 +0000

    Reworking source tree and Makefiles for out of tree builds.
    git-svn-id: https://svn.shibboleth.net/extensions/cpp-sp-resolver/trunk@290 3ebe4818-3638-0410-822d-ece5924dabe2

--- config_win32.h
+++ config_win32.h
@@ -66,13 +66,13 @@
 #define PACKAGE_NAME "shibboleth-resolver"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "shibboleth-resolver 0.1"
+#define PACKAGE_STRING "shibboleth-resolver 0.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "shibboleth-resolver"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1"
+#define PACKAGE_VERSION "0.2"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -85,7 +85,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "0.1"
+#define VERSION "0.2"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
